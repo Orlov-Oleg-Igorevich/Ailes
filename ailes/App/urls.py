15802120700c9b50290efa90797fec4a16f6e698
Oleg <oleg_orlov_2005@mail.ru>
2024-03-08 21:17:19 +0300

    fixed links

--- ailes/App/urls.py
+++ ailes/App/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("", views.about),
-    path("", views.contact),
-    path("", views.News),
-    path("", views.signup)
+    path("", views.index, name='home'),
+    path("News/", views.News, name='news'),
+    path("contact/", views.contact, name='contact'),
+    path("about/", views.about, name='about'),
+    path("signup/", views.signup, name='signup')
 ]
