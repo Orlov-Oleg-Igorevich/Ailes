3d5a33f7b7e0f3098ddac7d2d766e9d8d4ba5d86
zomberep <zomberep05@gmail.com>
2024-03-09 22:14:56 +0300

    Data Base

--- ailes/Social_networks/models.py
+++ ailes/Social_networks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from functools import reduce
 
-# Create your models here.
+class MainInfo(models.Model):
+    login = models.BinaryField()
+    password = models.BinaryField()
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+
+
+
+
