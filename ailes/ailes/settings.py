6589e2adf796b7a917ecee8d3b6fd4392757aeed
sergei_dobri <teplichkinteplichkinov@gmail.com>
2024-03-15 22:06:10 +0300

    new virtual environment, renaming app class, change settings.py a bit

--- ailes/ailes/settings.py
+++ ailes/ailes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Social_networks.apps.SocialNetworksConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'ailes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["Social_networks/templates/"],
+        'DIRS': [
+            BASE_DIR / 'Social_networks' / 'templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
